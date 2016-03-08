de7dfa0c5f375ef39929c6fa019f70c91a252a6f
Drunar <andreascml@gmail.com>
2016-03-08 09:43:16 +0100

    Changes to LightningUnit
    1. implementation af lightingUnit

--- MapController/Triangulate/LightingUnit.cs
+++ MapController/Triangulate/LightingUnit.cs
@@ -9,11 +9,65 @@ namespace Triangulering
 {
     class LightingUnit : Coords
     {
-        public double LightingLevel;
+        float stepInterval = 0.5f;
+        int adress;
+        float maxLevel = 1.0f;
+        float minLevel = 0.0f;
+        float LightingLevel = 0.0f;
+        int[] group = new int[4];
+        float[] scene = new float[16];
 
-        public void SetLightLevel(double InputLightingLevel)
+
+        public LightingUnit(int address, double X, double Y) : this(address, X, Y, null, null)
         {
-            LightingLevel = InputLightingLevel;
+            x = X;
+            y = Y;
         }
+
+        public LightingUnit(int address, double X, double Y, int[] group, float[] scene)
+        {
+            x = X;
+            y = Y;
+        }
+
+
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+
+        public float goToMax()
+        {
+            LightingLevel = maxLevel;
+            return LightingLevel;
+        }
+
+        public float goToMin()
+        {
+            LightingLevel = minLevel;
+            return LightingLevel;
+        }
+
+        public float stepUp()
+        {
+            LightingLevel = LightingLevel + stepInterval;
+            return LightingLevel;
+        }
+
+        public float stepDown()
+        {
+            LightingLevel = LightingLevel - stepInterval;
+            return LightingLevel;
+        }
+
     }
 }
