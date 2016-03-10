a4b82df99d66f90dea48387fc4b971b223f00458
Drunar <andreascml@gmail.com>
2016-03-10 08:40:02 +0100

    Changes and new functions in LightingUnit

--- MapController/Triangulate/LightingUnit.cs
+++ MapController/Triangulate/LightingUnit.cs
@@ -9,28 +9,89 @@ namespace Triangulering
 {
     class LightingUnit : Coords
     {
-        double stepInterval = 0.05;
-        double maxLevel = 1.0;
-        double minLevel = 0.0;
-        public double LightingLevel = 0.0;
-        int[] group = new int[4];
-        double[] scene = new double[16];
+        double stepInterval = 0.01; //intervallet hvormed der bliver ændret ved stepUp og stepDown
+        double maxLevel = 1.0; //max lysstyrke (basically en standard on knap)
+        double minLevel = 0.0; //min lysstyrke (basically en standard off knap)
+        private int _address = 0;
+        private double fadeRate = 0.5; //jeg har en ide med den her (kan måske undgåes at skulle sammenligne to lister for at fade lamperne ud efter brug)
+        private double watts = 60; //skal bruges i udregninger til strømforbrug (har gemt et link jeg gerne lige vil snakke om :))
+        public double LightingLevel = 0.0; //Lampens nuværende lysniveau  (skal måske laves til private hvis daliCommands skal køres(forklaring følger))
+        List<int> groups = new List<int>(); //liste over grupper den enkelte light unit tilhører
+        double[] scene = new double[16] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0}; //array af presets (her tænker jeg vi laver nogle standard scener 
+        //der gælder for alle light units
 
 
-        public LightingUnit() :this(0, 0, null, null)
+        public LightingUnit() :this(0, 0) //for testing purposes only. will be deleted later
         {
         }
 
-        public LightingUnit(double X, double Y) : this(X, Y, null, null)
+        public LightingUnit(double X, double Y) //constructor for lightingUnit (modtager x og y og skaber en light unit med x, y samt en adresse/id
         {
             x = X;
             y = Y;
+            Address = _address;
+            _address++;
+            //her skal vi have lavet en sikkerhedsforanstaltning der starter en ny liste når _address når 63
+
         }
 
-        public LightingUnit(double X, double Y, int[] group, float[] scene)
+        public void addLightUnitToGroup(int groupToAdd) //tilføjer en lightingUnit til en gruppe
         {
-            x = X;
-            y = Y;
+
+            if (groups.Contains(groupToAdd))
+            {
+                //her skal laves en rigtig exeption
+                Console.WriteLine("The lighting unit is already part of that group");
+            }
+
+            else if (groups.Count() >= 4)
+            {
+                //exeption
+                Console.WriteLine("There is currently no room to add another group please delete one before adding another");
+            }
+
+            else if (!groups.Contains(groupToAdd) && groups.Count() < 4) //her bliver 
+            {
+                groups.Add(groupToAdd);
+            }
+
+            else
+            {
+                Console.WriteLine("Something went completely wrong trying to add the light unit to another group");
+            }
+          
+
+        }
+
+        public void removeLightUnitFromGroup(int groupToRemove) //fjerner en gruppe fra en LightingUnit
+        {
+            if (!groups.Contains(groupToRemove))
+            {
+                //igen her skal laves en bedre exeption
+                Console.WriteLine("The lighting unit is not a member of that group");
+            }
+            else if (groups.Contains(groupToRemove))
+            {
+                groups.Remove(groupToRemove);
+            }
+            else
+            {
+                //bedre exeption
+                Console.WriteLine("Something went completely wrong trying to remove the light unit to another group");
+            }
+
+
+        }
+
+        public void clearGroupsFromLightingUnit(LightingUnit LightingUnitToClearGroupsFrom) //blot en simpel funktion til at rense grupperne i en Light Unit
+        {
+            LightingUnitToClearGroupsFrom.groups.Clear();
+        }
+
+        public int Address
+        {
+            get { return _address; }
+            private set { _address = value; }
         }
 
 
@@ -58,5 +119,22 @@ namespace Triangulering
             return LightingLevel;
         }
 
+        public void Extinguish(LightingUnit lightingUnitToExtinguish) //sluk med det samme!!!
+        {
+            lightingUnitToExtinguish.LightingLevel = 0;
+        }
+        /*public double setLightLevel(double wantedLightLevel)
+        {
+            while (LightingLevel > wantedLightLevel)
+            {
+                stepUp();
+            }
+            while (LightingLevel < wantedLightLevel)
+            {
+                stepUp();
+            }
+            return LightingLevel;
+        }
+        */
     }
 }
