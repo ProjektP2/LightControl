8d224e9eba6bcc4d35725f3381479e13c9c221fd
Mysjkin <iarogolo@gmail.com>
2016-03-14 09:08:04 +0100

    Quadtree uden intersection

---
+++ MapController/Quadtree/Bounds.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightControl;
+
+namespace MapController.QuadTree
+{
+    class Bounds : Coords
+    {
+        public int Width;
+        public int Height;
+        private int CenterX, CenterY;
+        private int TopRightX, TopRightY;
+
+        public Bounds(double x, double y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            Width = width;
+            Height = height;
+            TopRightX = (int)x + width;
+            TopRightY = (int)y;
+        }
+
+        public bool Contains(Bounds bound)
+        {
+            return bound.x >= x && bound.y >= y && 
+                bound.TopRightX <= TopRightX && 
+                bound.TopRightY <= TopRightY;
+        }
+
+        public bool Intersects(Bounds bound)
+        {
+            return (bound.x >= TopRightX || bound.TopRightX <= x ||
+                bound.y >= TopRightY || bound.TopRightY <= y); 
+        }
+    }
+}
